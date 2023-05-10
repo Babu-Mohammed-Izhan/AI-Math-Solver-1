34904fa4ed9e6b01b7effdc85f93f1a43bad7dea
Babu Izhan <babu.izhan@shopup.org>
2023-05-11 02:10:10 +0530

    remove header

--- flask-server/test.py
+++ flask-server/test.py
@@ -8,8 +8,5 @@ class TestSum(unittest.TestCase):
     def test_not_ai(self):
         self.assertEqual(ai(json.loads('{ "message": "This is not a math equation", "history" : [] }')), "Sorry, this is a invalid question. How can I help you with math?", "Should be a invalid math equation")
 
-    def test_ai(self):
-        self.assertIn("[[4, 5, 7], [6, 9, 11]]",ai(json.loads('{ "message": "Add [[2,5,7], [4,7,9]] and [[2,0,0], [2,2,2]]", "history" : [] }')), "Should be able to solve the maths equation")
-
 if __name__ == '__main__':
     unittest.main()
